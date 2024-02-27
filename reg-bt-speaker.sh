7654fecd3022f848c7896713408435a5def89264
ironwin <ironwin2@gmail.com>
2024-02-27 16:42:30 +0900

    bts

--- reg-bt-speaker.sh
+++ reg-bt-speaker.sh
@@ -3,32 +3,55 @@
 BTSP="67:24:34:34:CE:48"
 BTLT="/tmp/bts.list"
 
-# truncate
-cat /dev/null > ${BTLT}
 
+# $? : 0 [success]
+#      other [fail]
 
 check_bluetooth_service() {
     systemctl is-active bluetooth.service >/dev/null 2>&1
     return $?
 }
 
+check_speaker() {
+  grep -q ${BTSP} ${BTLT}
+}
+
+check_paried() {
+  bluetoothctl paired-devices | grep ${BTSP}
+  return $?
+}
+
 reg_speaker() {
 
-  bluetoothctl remove ${BTSP}
-  stdbuf -oL bluetoothctl --timeout 120 scan on > ${BTLT} 
-  bluetoothctl connect ${BTSP}
-  logger "success register bluetooth speaker ${BLSP}"
+  logger "regist bluetooth speeker ${BTSP}"
 
+  # truncate list
+  cat /dev/null > ${BTLT}
+
+  # remove first 
+  bluetoothctl remove ${BTSP}
+
+  # scan on for $1 min
+  stdbuf -oL bluetoothctl --timeout $1 scan on > ${BTLT} 
+
+  if !check_speaker; then
+      logger "no ${BTSP} in ${BTLT}"
+  else
+      bluetoothctl connect ${BTSP}
+      logger "success register bluetooth speaker ${BLSP}"
+      return 0
+  fi 
+  return 1
 }
 
 # 300초 동안 반복해서 Bluetooth 서비스 체크
 WTIME=300
 for ((i=0; i<WTIME; i++)); do
     if check_bluetooth_service; then
-        reg_speaker
-        exit 0
-    else
         logger "wait bluetooth service active $i/$WTIME"
+    else
+        reg_speaker 180
+        exit 0
     fi
     sleep 1
 done
