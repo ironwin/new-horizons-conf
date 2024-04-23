69a7e45e83dc045cbcfa194062b34ac791414702
ironwin <ironwin2@gmail.com>
2024-04-23 17:00:37 +0900

    4/23

--- mm.sh
+++ mm.sh
@@ -10,15 +10,26 @@ nowh=$(date +"%H")
 cd /home/pi/MagicMirror
 logger "mm.sh > magicmirror : ${dow} ${nowh}"
 
-if [[ "${dow}" == "6" || "${dow}" == "7" ]]; then
+# sunday evening over 19 -> local-photo (JWST)
+if [[ "${dow}" == "7" && "${nowh}" -gt 18 ]]; then
+    logger "mm.sh > localphoto.ftp"
+    cp ./config/config.js.ftp ./config/config.js
+#
+# sunday, sat : googlephtots    
+elif [[ "${dow}" == "6" || "${dow}" == "7" ]]; then
     logger "mm.sh > googlephoto"
     $(${csh})
+#
+# friday 12 : googlephotos
 elif [[ "${dow}" == "5" && "${nowh}" -gt 12 ]]; then
     logger "mm.sh > googlephoto"
     $(${csh})
+#
+# weekday over 18 : googlephoto
 elif [ "${nowh}" -gt 18 ]; then
     logger "mm.sh > googlephoto"
     $(${csh})
+#
 else
     logger "mm.sh > calendor"
     cp ./config/config.js.common ./config/config.js
