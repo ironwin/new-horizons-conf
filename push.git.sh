39d14b4d511667dd7268ae4f8ed274e2c323fe37
ironwin <ironwin2@gmail.com>
2024-11-18 14:49:25 +0900

    test

---
+++ push.git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# work path
+WORK=/home/pi/new-horizons-conf
+TODAY=$(date '+%Y-%m-%d')
+THIS=$1
+cd $WORK
+
+git pull
+
+# Backup ./raspi/....
+crontab -l > ./raspi/$THIS/$THIS.crontab
+cp ~/.profile ./raspi/$THIS/$THIS.profile
+
+git commit -m "$TODAY updated"
+git push
+
+
+
